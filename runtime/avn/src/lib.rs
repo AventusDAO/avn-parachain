904a56d5b714469ae256d42ddc427a8fb03e2264
nahuseyoum <nahu.seyoum@aventus.io>
2026-02-13 14:02:09 +0000

    feat: enable async backing again

--- runtime/avn/src/lib.rs
+++ runtime/avn/src/lib.rs
@@ -170,9 +170,9 @@ const AVERAGE_ON_INITIALIZE_RATIO: Perbill = Perbill::from_percent(5);
 const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
 
 #[docify::export(max_block_weight)]
-/// We allow for 0.5 of a second of compute with a 12 second average block time.
+/// We allow for 2 seconds of compute with a 12 second average block time.
 const MAXIMUM_BLOCK_WEIGHT: Weight = Weight::from_parts(
-    WEIGHT_REF_TIME_PER_SECOND.saturating_div(2),
+    WEIGHT_REF_TIME_PER_SECOND.saturating_mul(2),
     cumulus_primitives_core::relay_chain::MAX_POV_SIZE as u64,
 );
 
@@ -182,7 +182,7 @@ mod async_backing_params {
     // 1 = sync-ready but no pipelining (effectively DISABLED).
     // > 1 asynchronous backing is ENABLED.
     // Must not exceed relay max_candidate_depth.
-    pub(crate) const UNINCLUDED_SEGMENT_CAPACITY: u32 = 1;
+    pub(crate) const UNINCLUDED_SEGMENT_CAPACITY: u32 = 3;
     /// How many parachain blocks are processed by the relay chain per parent. Limits the
     /// number of blocks authored per slot.
     pub(crate) const BLOCK_PROCESSING_VELOCITY: u32 = 1;
