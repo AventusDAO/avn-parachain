9205eb6251fdd04ff4333bf5fb6c07478f839d9d
nahuseyoum <nahu.seyoum@aventus.io>
2026-03-23 10:04:37 +0000

    fix: keep event enums the same

--- pallets/avn-anchor/src/lib.rs
+++ pallets/avn-anchor/src/lib.rs
@@ -169,13 +169,13 @@ pub mod pallet {
         UnauthorizedHandler,
         NoAvailableChainId,
         EmptyChainName,
-        /// The token symbol provided for the app chain is empty.
-        EmptyTokenSymbol,
         NoAvailableCheckpointId,
         UnauthorizedSignedTransaction,
         SenderNotValid,
         TransactionNotSupported,
         UnauthorizedProxyTransaction,
+        // Deprecated, keeping so indexes don't break
+        _NoChainDataAvailable,
         CheckpointOriginAlreadyExists,
         /// The app chain already has a token registered.
         AppChainTokenAlreadyRegistered,
@@ -191,6 +191,8 @@ pub mod pallet {
         TokenLocationAlreadyRegistered,
         /// This extrinsic has been deprecated and is no longer available.
         CallDeprecated,
+        /// The token symbol provided for the app chain is empty.
+        EmptyTokenSymbol,
     }
 
     #[pallet::storage]
